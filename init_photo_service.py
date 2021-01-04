b78d2ca64b3641880183b7fb10338337069a2c72
thomasmburke <tburke@bu.edu>
2021-01-04 15:39:57 -0800

    updated location of credentials file

--- init_photo_service.py
+++ init_photo_service.py
@@ -4,7 +4,7 @@ from google_photo_ops import Create_Service
 
 API_NAME = 'photoslibrary'
 API_VERSION = 'v1'
-CLIENT_SECRET_FILE = '/Users/tomburke/Downloads/google_photo_frame_OAuth.json'
+CLIENT_SECRET_FILE = 'google_photo_frame_OAuth.json'
 SCOPES = ['https://www.googleapis.com/auth/photoslibrary',
           'https://www.googleapis.com/auth/photoslibrary.sharing']
  
