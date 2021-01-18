94ae971931f3ff6be26053d8c2a0e003a9534234
thomasmburke <tburke@bu.edu>
2021-01-18 12:45:29 -0800

    further building on scrappy POC

--- demo_albums.py
+++ demo_albums.py
@@ -1,5 +1,8 @@
+from io import BytesIO
+import json
+
 import requests
-from PIL import Image
+from PIL import Image, ImageTk
 
 from init_photo_service import service
 
@@ -41,8 +44,45 @@ request_body = {
 
 lstMediaItems = response_media_items_by_filter(request_body=request_body)
 
-print(lstMediaItems)
+print(json.dumps(lstMediaItems, indent=4))
 
 # https://stackoverflow.com/a/23489503/9586164
+# response = requests.get(url)
+# img = Image.open(response.raw)
+for mediaItem in lstMediaItems:
+    if ('JPG' in mediaItem['filename']):
+        url = mediaItem['baseUrl']
+
+# img = Image.open(BytesIO(response.content))
+
+import sys
+if sys.version_info[0] == 2:  # the tkinter library changed it's name from Python 2 to 3.
+    import Tkinter
+    tkinter = Tkinter #I decided to use a library reference to avoid potential naming conflicts with people's programs.
+else:
+    import tkinter
+
+def showPIL(pilImage):
+    root = tkinter.Tk()
+    w, h = root.winfo_screenwidth(), root.winfo_screenheight()
+    root.overrideredirect(1)
+    root.geometry("%dx%d+0+0" % (w, h))
+    root.focus_set()    
+    root.bind("<Escape>", lambda e: (e.widget.withdraw(), e.widget.quit()))
+    canvas = tkinter.Canvas(root,width=w,height=h)
+    canvas.pack()
+    canvas.configure(background='black')
+    imgWidth, imgHeight = pilImage.size
+    if imgWidth > w or imgHeight > h:
+        ratio = min(w/imgWidth, h/imgHeight)
+        imgWidth = int(imgWidth*ratio)
+        imgHeight = int(imgHeight*ratio)
+        pilImage = pilImage.resize((imgWidth,imgHeight), Image.ANTIALIAS)
+    image = ImageTk.PhotoImage(pilImage)
+    imagesprite = canvas.create_image(w/2,h/2,image=image)
+    root.mainloop()
+
+
 response = requests.get(url)
-img = Image.open(response.raw)
+pilImage = Image.open(BytesIO(response.content))
+showPIL(pilImage)
